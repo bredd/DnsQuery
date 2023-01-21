ea06412a58264dc306f39f29d61b59fc0e71adfb
Brandt Redd <brandt@redd.org>
2023-01-20 17:45:54 -0700

    Add project files.

---
+++ DnsQuery.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Bredd
+{
+    /// <summary>
+    /// Perform DNS queries for MX and TXT records via p/invoke to Win32 DnsQuery API
+    /// </summary>
+    /// <remarks>
+    /// <para>Only works on Windows platforms.
+    /// </para>
+    /// <para>See https://learn.microsoft.com/en-us/windows/win32/api/windns/
+    /// </para>
+    /// </remarks>
+    internal class WinDnsQuery
+    {
+        /// <summary>
+        /// Retrieve TXT records for the specified domain.
+        /// </summary>
+        /// <param name="domainName">Name of the domain on which to perform the query.</param>
+        /// <returns>An array of TXT records</returns>
+        public static string[]? GetTxtRecords(string domainName)
+        {
+            IntPtr pDnsRecord = IntPtr.Zero;
+            try
+            {
+                var result = new List<string>();
+
+                Int32 hresult = DnsQuery(domainName, QueryTypes.DNS_TYPE_TEXT, QueryOptions.DNS_QUERY_RETURN_MESSAGE, IntPtr.Zero, ref pDnsRecord, IntPtr.Zero);
+                if (hresult == (Int32)DnsError.DNS_ERROR_RCODE_NAME_ERROR) return null;
+                if (hresult != 0) throw new Win32Exception(hresult);
+
+                IntPtr pCurrent = pDnsRecord;
+                while(pCurrent != IntPtr.Zero)
+                {
+                    var rec = Marshal.PtrToStructure<DNS_RECORDW>(pCurrent);
+#if DEBUG
+                    Debug.WriteLine(Marshal.PtrToStringUni(rec.pName));
+#endif
+                    if (rec.wType == (UInt16)QueryTypes.DNS_TYPE_TEXT)
+                    {
+                        int stringCount = Marshal.ReadInt32(pCurrent + DNS_RECORDW_SIZE);
+                        for (int i=0; i<stringCount; ++i)
+                        {
+                            IntPtr pString = Marshal.ReadIntPtr(pCurrent + DNS_RECORDW_SIZE + ((i+1) * IntPtr.Size));
+                            result.Add(Marshal.PtrToStringUni(pString) ?? string.Empty);
+                        }
+                    }
+                    pCurrent = rec.pNext;
+                }
+                return result.ToArray();
+            }
+            finally
+            {
+                if (pDnsRecord != IntPtr.Zero) DnsRecordListFree(pDnsRecord, 0);
+            }
+        }
+
+        // See https://learn.microsoft.com/en-us/windows/win32/api/windns/nf-windns-dnsquery_w
+        [DllImport("dnsapi", EntryPoint = "DnsQuery_W", CharSet = CharSet.Unicode, SetLastError = true, ExactSpelling = true)]
+        private static extern Int32 DnsQuery([MarshalAs(UnmanagedType.LPWStr)] string pszName, [In] QueryTypes wType, [In] QueryOptions options, [In] IntPtr pExtra, [In, Out] ref IntPtr ppQueryResults, [In] IntPtr pReserved);
+
+        // See https://learn.microsoft.com/en-us/windows/win32/api/windns/nf-windns-dnsrecordlistfree
+        [DllImport("dnsapi", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern void DnsRecordListFree(IntPtr pRecordList, Int32 FreeType);
+
+        // See: https://learn.microsoft.com/en-us/windows/win32/dns/dns-constants
+        private enum QueryTypes : UInt16
+        {
+            DNS_TYPE_A = 0x0001,
+            DNS_TYPE_MX = 0x000f,
+            DNS_TYPE_TEXT = 0x0010,
+            DNS_TYPE_SRV = 0x0021
+        }
+
+        // See: https://learn.microsoft.com/en-us/windows/win32/dns/dns-constants
+        private enum QueryOptions : UInt32
+        {
+            DNS_QUERY_STANDARD         = 0x00000000,
+            DNS_QUERY_ACCEPT_TRUNCATED_RESPONSE = 0x00000001,
+            DNS_QUERY_USE_TCP_ONLY     = 0x00000002,
+            DNS_QUERY_NO_RECURSION     = 0x00000004,
+            DNS_QUERY_BYPASS_CACHE     = 0x00000008,
+            DNS_QUERY_NO_WIRE_QUERY    = 0x00000010,
+            DNS_QUERY_NO_LOCAL_NAME    = 0x00000020,
+            DNS_QUERY_NO_HOSTS_FILE    = 0x00000040,
+            DNS_QUERY_NO_NETBT         = 0x00000080,
+            DNS_QUERY_WIRE_ONLY        = 0x00000100,
+            DNS_QUERY_RETURN_MESSAGE   = 0x00000200,
+            DNS_QUERY_MULTICAST_ONLY   = 0x00000400,
+            DNS_QUERY_NO_MULTICAST     = 0x00000800,
+            DNS_QUERY_TREAT_AS_FQDN    = 0x00001000,
+            DNS_QUERY_ADDRCONFIG       = 0x00002000,
+            DNS_QUERY_DUAL_ADDR        = 0x00004000,
+            DNS_QUERY_MULTICAST_WAIT   = 0x00020000,
+            DNS_QUERY_MULTICAST_VERIFY = 0x00040000,
+            DNS_QUERY_DONT_RESET_TTL_VALUES = 0x00100000,
+            DNS_QUERY_DISABLE_IDN_ENCODING = 0x00200000,
+            DNS_QUERY_APPEND_MULTILABEL = 0x00800000
+        }
+
+        private enum DnsError : Int32
+        {
+            DNS_ERROR_RCODE_FORMAT_ERROR = 9001,
+            DNS_ERROR_RCODE_SERVER_FAILURE = 9002,
+            DNS_ERROR_RCODE_NAME_ERROR = 9003,
+            DNS_ERROR_RCODE_NOT_IMPLEMENTED = 9004,
+            DNS_ERROR_RCODE_REFUSED = 9005,
+            DNS_ERROR_RCODE_YXDOMAIN = 9006,
+            DNS_ERROR_RCODE_YXRRSET = 9007,
+            DNS_ERROR_RCODE_NXRRSET = 9008,
+            DNS_ERROR_RCODE_NOTAUTH = 9009,
+            DNS_ERROR_RCODE_NOTZONE = 9010,
+            DNS_ERROR_RCODE_BADSIG = 9016,
+            DNS_ERROR_RCODE_BADKEY = 9017,
+            DNS_ERROR_RCODE_BADTIME = 9018
+        }
+
+        // See https://learn.microsoft.com/en-us/windows/win32/api/windns/ns-windns-dns_recordw
+        [StructLayout(LayoutKind.Sequential)]
+        private struct DNS_RECORDW
+        {
+            public IntPtr pNext;
+            public IntPtr pName;
+            public UInt16 wType;
+            public UInt16 wDataLength;
+            public UInt32 flags;
+            public UInt32 dwTtl;
+            public UInt32 dwReserved;
+        }
+
+        static readonly int DNS_RECORDW_SIZE = IntPtr.Size * 2 + 16;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine(IntPtr.Size.ToString());
+var list = Bredd.WinDnsQuery.GetTxtRecords("_dir.bredd.tech");
+if (list == null)
+{
+    Console.WriteLine("Not Found");
+}
+else
+{
+    foreach (var str in list)
+    {
+        Console.WriteLine(str);
+    }
+}
