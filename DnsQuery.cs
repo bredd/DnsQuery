6ede69480713d8398fa2eb15856864f7e8ebeccf
Brandt Redd <brandt@redd.org>
2023-01-21 12:51:31 -0700

    Add CodeBit detail and ReadMe

--- DnsQuery.cs
+++ DnsQuery.cs
@@ -1,11 +1,47 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿/*
+CodeBit Metadata
+&name=bredd.tech/DnsQuery.cs
+&description="CodeBit class for performing DNS Queries for TXT records."
+&author="Brandt Redd"
+&url=https://raw.githubusercontent.com/bredd/DnsQuery/main/DnsQuery.cs
+&version=1.0
+&keywords=CodeBit
+&dateModified=2023-01-21
+&license=https://opensource.org/licenses/BSD-3-Clause
+&comment="This could easily be extended to support other DNS record types."
+
+About Codebits http://www.filemeta.org/CodeBit
+*/
+
+/*
+=== BSD 3 Clause License ===
+https://opensource.org/licenses/BSD-3-Clause
+Copyright 2021-2022 Brandt Redd
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+1. Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+3. Neither the name of the copyright holder nor the names of its contributors
+may be used to endorse or promote products derived from this software without
+specific prior written permission.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+*/
+using System;
 using System.ComponentModel;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Diagnostics;
 
 namespace Bredd
@@ -14,7 +50,10 @@ namespace Bredd
     /// Perform DNS queries for MX and TXT records via p/invoke to Win32 DnsQuery API
     /// </summary>
     /// <remarks>
-    /// <para>Only works on Windows platforms.
+    /// <para>Only works on Windows platforms due to p/invoke to the operating system.
+    /// To make this a .net Core class that works across platforms the DNS protocol
+    /// should be implemented upon the sockets layer. For security, consider using
+    /// the DNS over HTTPS (DoH) protocol.
     /// </para>
     /// <para>See https://learn.microsoft.com/en-us/windows/win32/api/windns/
     /// </para>
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # DnsQuery
+This is a C# Class to perform DNS lookups of TXT records. It would be easy to adapt to read other
+record types such as MX, SRV, or PTR.
+
+Distributed as a [CodeBit](http://www.filemeta.org/CodeBit) for easy incorporation into projects.
+
+# License
+Licensed under a BSD 3-clause license.
+
+# Limitations and Opportunities
+The DNS lookup is implemented through p/invoke to the Win32
+[DnsQuery](https://learn.microsoft.com/en-us/windows/win32/api/windns/nf-windns-dnsquery_w)
+system call. Therefore, it only works only on Windows. So, while this code can be complied
+for .Net Core or .Net Framework, it will only work when running on a Windows operating system.
+
+A cross-platform .Net Core version would require implementing a DNS client rather than
+relying on the operating system. A promising option would be to use DNS over Https (DoH)
+thereby achieving greater security. Such an implementation would be limited to
+DoH servers but there are several available such as
+[CloudFlare DoH](https://developers.cloudflare.com/1.1.1.1/encryption/dns-over-https/make-api-requests),
+[Google Public DNS](https://developers.google.com/speed/public-dns/docs/doh/), and
+[Many Others](https://dnscrypt.info/public-servers/).
